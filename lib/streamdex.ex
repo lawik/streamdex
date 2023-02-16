722901ce5cc68a48ac2df2390c78cc05267ad197
Lars Wikman <lars@underjord.io>
2023-02-16 15:39:38 +0100

    Start experimenting

---
+++ lib/streamdex.ex
@@ -0,0 +1,18 @@
+defmodule Streamdex do
+  @moduledoc """
+  Documentation for `Streamdex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Streamdex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/streamdex_test.exs
@@ -0,0 +1,8 @@
+defmodule StreamdexTest do
+  use ExUnit.Case
+  doctest Streamdex
+
+  test "greets the world" do
+    assert Streamdex.hello() == :world
+  end
+end
